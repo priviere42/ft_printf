52e88c8c7633a142b946c906cb3dfd9bae4f1aad
Paul Riviere <priviere@z4r1p6.le-101.fr>
2020-01-10 15:57:15 +0100

    managing *, reduced function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: priviere <priviere@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 15:12:22 by priviere     #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/08 18:06:02 by priviere    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/10 11:06:20 by priviere    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -32,6 +32,7 @@ int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_itoa(int n);
 char	*ft_itoa_base(int value, int base);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: priviere <priviere@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/12/18 09:41:57 by priviere     #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/08 19:11:39 by priviere    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/10 15:56:22 by priviere    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -121,33 +121,50 @@ void my_printf_nbr(va_list my_list, t_params *par)
 	}
 }
 
-int	ft_check_flags(const char *src, int i, t_params *par)
+int ft_check_wildcard(va_list my_list, const char *src, int i, t_params *par)
 {
+	int num;
+	
+	if (src[i] == '*')
+	{
+		if ((num = va_arg(my_list, int)) < 0)
+		{
+			par->width = -1 * num;
+			par->flag = '-';
+		}
+		else
+			par->width = num;
+		i++;
+	}
+	if (src[i] == '.' && src[i + 1] == '*')
+	{
+		par->precision = va_arg(my_list, int);
+		i = i + 2;
+	}
+	return (i);
+}
+
+int	ft_check_flags(va_list my_list, const char *src, int i, t_params *par)
+{
+	int num;
+
 	if (src[i] == '0' || src[i] == '-')
 	{
 		par->flag = (src[i] == '0') && (src[i + 1] == '-') ? '-' : src[i];
-		if ((src[i] == '0') && (src[i + 1] == '-'))
+		if (((src[i] == '0') && (src[i + 1] == '-')) || ((src[i] == '-') && (src[i + 1] == '0')))
 			i++;
 		i++;
 	}
+	i = ft_check_wildcard(my_list, src, i, par);
 	if (src[i] > '0' && src[i] <= '9')
 		par->width = ft_atoi(&src[i]);
-	if ((src[i] == '.' || src[i] == '*') && src[i + 1])
-	{
-		par->precision = ft_atoi(&src[i + 1]);
-		i++;
-		while (src[i] && (src[i] >= '0' && src[i] <= '9'))
-			i++;
-	}
+	if ((src[i] == '.' && src[i + 1]))
+		par->precision = ft_atoi(&src[++i]);
 	if ((par->flag != 'a') && src[i + 1] >= '0' && src[i + 1] <= '9')
-	{
-		par->width = ft_atoi(&src[i]);
+		par->width = ft_atoi(&src[i++]);
+	while (src[i] && (src[i] >= '0' && src[i] <= '9'))
 		i++;
-		while (src[i] && (src[i] >= '0' && src[i] <= '9'))
-			i++;
-	}
-//	i++;
-	//printf("par->width = %d,\n par->flag = %c,\n par->precision = %d\n", par->width, par->flag, par->precision);
+//	printf("par->width = %d, par->flag = %c, par->precision = %d, index = %i\n", par->width, par->flag, par->precision, i);
 	return (i);
 }
 
@@ -168,8 +185,8 @@ void ft_printf(const char *src, ...)
 		par->flag = 'a';
         if (src[i] != 0 && i != 0 && src[i - 1] == '%')
         {
-			i = ft_check_flags(src, i, par);
-	//		printf("\npar->width = %d, par->flag = %c, par->precision = %d, index = %d, src[i] = %c\n", par->width, par->flag, par->precision, i, src[i]);
+			i = ft_check_flags(my_list, src, i, par);
+		//	printf("\npar->width = %d, par->flag = %c, par->precision = %d, index = %d, src[i] = %c\n", par->width, par->flag, par->precision, i, src[i]);
             if (src[i] == 'd')
                 my_printf_nbr(my_list, par);
             if (src[i] == 's')
@@ -192,8 +209,8 @@ void ft_printf(const char *src, ...)
 
 int main(int ac, char **argv)
 {
-    ft_printf("Mon printf : %s %s %.10d hello %% %.12s salut \n", "nani", "chaine 1 ", 14250, "chaine de caracteres");
-       printf("The printf : %s %s %.10d hello %% %.12s salut \n", "nani", "chaine 1 ", 14250, "chaine de caracteres");
+    ft_printf("Mon printf : %s %s %.020d hello %% %.12s salut \n", "nani", "chaine 1 ", 1, "chaine de caracteres");
+       printf("The printf : %s %s %.020d hello %% %.12s salut \n", "nani", "chaine 1 ", 1, "chaine de caracteres");
 	
     return (0);
 }
